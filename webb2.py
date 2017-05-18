aeaa4a7fd0489e11522687e900897958cff55274
qqii <Qi-rui.Chen.2015@my.bristol.ac.uk>
2017-05-18 21:52:59 +0100

    Bot echos any other text

--- webb2.py
+++ webb2.py
@@ -11,9 +11,13 @@ dispatcher = updater.dispatcher
 
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
-
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
+
+def echo(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+echo_handler = MessageHandler(Filters.text, echo)
+dispatcher.add_handler(echo_handler)
 
 updater.start_polling()
 
