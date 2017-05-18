26c03ee7b667f4caa245b06406dc8a20ae6d1a51
qqii <Qi-rui.Chen.2015@my.bristol.ac.uk>
2017-05-19 00:05:25 +0100

    James knows when converstations end

--- webb2.py
+++ webb2.py
@@ -2,6 +2,8 @@ from telegram.ext import *
 import mimetypes
 import random
 import time
+import datetime
+from collections import defaultdict
 
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
@@ -20,10 +22,10 @@ def reply(bot, update):
     # random number of emojis
     bot.send_message(chat_id=update.message.chat_id, text="\U0001F602" * random.randint(2, 3))
 
-def textimage(bot, update):
-    print("{}({}): {}".format(update.message.from_user.username, 
-                              update.message.date,
-                              update.message.text))
+def text(bot, update):
+    # print("{}({}): {}".format(update.message.from_user.username, 
+                            #   update.message.date,
+                            #   update.message.text))
     mt, _ = mimetypes.guess_type(update.message.text)
     if (mt and mt.startswith("image")) or (update.message.text.find("imgur.com") != -1):
         reply(bot, update)
@@ -34,21 +36,32 @@ def photo(bot, update):
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
 
+lm = defaultdict(lambda: datetime.datetime(2017, 5, 17))
 
+def message(bot, update):
+    global lm
+    
+    if (update.message.date - lm[update.message.chat.id] > datetime.timedelta(0, 20*60)):
+        if update.message.photo:
+            photo(bot, update)
+        else:
+            text(bot, update)
+    lm[update.message.chat.id] = update.message.date
+    
 def main():
     updater = Updater(key)
 
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(Filters.text, textimage))
-    dp.add_handler(MessageHandler(Filters.photo, photo))
+    
     dp.add_error_handler(error)
+    dp.add_handler(MessageHandler(Filters.all, message))
 
-    logger.info("Starting Webb Bot II")
+    logger.info("Starting...")
     updater.start_polling()
-
     updater.idle()
+    logger.info("Stopping...")
 
 
 if __name__ == '__main__':
