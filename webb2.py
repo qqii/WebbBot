cb96929a711c2bbd6922f3a7ccacd2d3884f37a6
qqii <Qi-rui.Chen.2015@my.bristol.ac.uk>
2017-05-18 23:26:22 +0100

    "\U0001F602"

--- webb2.py
+++ webb2.py
@@ -1,7 +1,9 @@
-import logging
 from telegram.ext import *
 import mimetypes
+import random
+import time
 
+import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger("Bot")
 
@@ -11,18 +13,23 @@ with open("key.txt", "r") as f:
 def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
+def reply(bot, update):
+    # http://homepage.divms.uiowa.edu/~mbognar/applets/beta.html
+    # peak near 3, human delay time
+    time.sleep(random.betavariate(8, 3) * 4)
+    # random number of emojis
+    bot.send_message(chat_id=update.message.chat_id, text="\U0001F602" * random.randint(2, 3))
+
 def textimage(bot, update):
     print("{}({}): {}".format(update.message.from_user.username, 
                               update.message.date,
                               update.message.text))
     mt, _ = mimetypes.guess_type(update.message.text)
-    if (mt and mt.startswith("image")):
-        bot.send_message(chat_id=update.message.chat_id, text="\U0001F602\U0001F602\U0001F602")
-    elif update.message.text.find("imgur.com") != -1:
-        bot.send_message(chat_id=update.message.chat_id, text="\U0001F602\U0001F602\U0001F602")
-
+    if (mt and mt.startswith("image")) or (update.message.text.find("imgur.com") != -1):
+        reply(bot, update)
+        
 def photo(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="\U0001F602\U0001F602\U0001F602")    
+    reply(bot, update)
 
 def error(bot, update, error):
     logger.warn('Update "%s" caused error "%s"' % (update, error))
